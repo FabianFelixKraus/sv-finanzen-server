34adc113b53143dd7a2dafc5304b4d7e7d031bf7
Fabian Kraus <kraus.4@campus.tu-berlin.de>
2023-05-04 21:14:44 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿require("dotenv").config()
+
+const express = require("express");
+const app = express()
+const mongoose = require("mongoose")
+const {request} = require("express");
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
+    .then(() => console.log("Connected to Database"))
+    .catch((error) => console.error("Database connection error:", error))
+
+app.use(express.json())
+
+const transactionsRouter = require("./routes/transactions")
+app.use("/transactions", transactionsRouter)
+
+
+const port = 3000
+app.listen(port, () => console.log(`Server started and listening on port: ${port}`))
